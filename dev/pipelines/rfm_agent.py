c8fe78aaed6f9299f07bc3d6bb940c8a2e1bb82f
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-18 15:16:52 +0000

    AI: joined datasets using customer_id in pipeline rfm_agent

--- dev/pipelines/rfm_agent.py
+++ dev/pipelines/rfm_agent.py
@@ -1,20 +1,6 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
-
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    ecom_orders = Task(
-        task_id = "ecom_orders", 
-        component = "Dataset", 
-        table = {"name" : "ecom_orders", "sourceType" : "Source", "sourceName" : "itai.retail_analyst", "alias" : ""}
-    )
-    instore_sales = Task(
-        task_id = "instore_sales", 
-        component = "Dataset", 
-        table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "itai.retail_analyst", "alias" : ""}
-    )
-    crm_customers = Task(
-        task_id = "crm_customers", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "itai.retail_analyst", "alias" : ""}
+with DAG():
+    rfm_agent__customer_id_join = Task(
+        task_id = "rfm_agent__customer_id_join", 
+        component = "Model", 
+        modelName = "rfm_agent__customer_id_join"
     )
