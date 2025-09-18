efeb098f0f412f6834d4a512b97c43f9b8a11b39
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-18 17:05:48 +0000

    User-performed data transformations and changes on pipeline rfm_demo

--- dev/pipelines/rfm_demo.py
+++ dev/pipelines/rfm_demo.py
@@ -1,6 +1,16 @@
 with DAG():
-    rfm_demo__customer_order_join = Task(
-        task_id = "rfm_demo__customer_order_join", 
-        component = "Model", 
-        modelName = "rfm_demo__customer_order_join"
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        table = {"name" : "crm_customers", "sourceType" : "Source", "sourceName" : "itai.retail_analyst"}
+    )
+    ecom_orders = Task(
+        task_id = "ecom_orders", 
+        component = "Dataset", 
+        table = {"name" : "ecom_orders", "sourceType" : "Table", "sourceName" : "itai.retail_analyst"}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        table = {"name" : "instore_sales", "sourceType" : "Table", "sourceName" : "itai.retail_analyst"}
     )
