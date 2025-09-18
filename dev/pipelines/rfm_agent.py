e12ababcc9bb686602441adae3bd1875075f3c26
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-18 15:00:35 +0000

    Initialize Customer Segmentation project with RFM agent pipeline and supporting macros and sources.

---
+++ dev/pipelines/rfm_agent.py
@@ -0,0 +1,14 @@
+Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
+SensorSchedule = SensorSchedule(enabled = False)
+
+with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    ecom_orders = Task(
+        task_id = "ecom_orders", 
+        component = "Dataset", 
+        table = {"name" : "ecom_orders", "sourceType" : "Source", "sourceName" : "itai.retail_analyst", "alias" : ""}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "itai.retail_analyst", "alias" : ""}
+    )
