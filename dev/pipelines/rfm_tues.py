7262da3400833144aa4c2b81960e41fcaec5b1f0
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-23 14:28:38 +0000

    User-performed data transformations and changes on pipeline rfm_tues

--- dev/pipelines/rfm_tues.py
+++ dev/pipelines/rfm_tues.py
@@ -1,6 +1,16 @@
 with DAG():
-    rfm_tues__customer_data_join = Task(
-        task_id = "rfm_tues__customer_data_join", 
-        component = "Model", 
-        modelName = "rfm_tues__customer_data_join"
+    ecom_orders = Task(
+        task_id = "ecom_orders", 
+        component = "Dataset", 
+        table = {"name" : "ecom_orders", "sourceType" : "Table", "sourceName" : "itai.retail_analyst"}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        table = {"name" : "instore_sales", "sourceType" : "Table", "sourceName" : "itai.retail_analyst"}
+    )
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        table = {"name" : "crm_customers", "sourceType" : "Source", "sourceName" : "itai.retail_analyst"}
     )
