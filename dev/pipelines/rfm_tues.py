5db01f6794a37a80bb2a6d52d7987c745c3d5115
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-23 14:19:04 +0000

    User-performed data transformations and changes on pipeline rfm_tues

--- dev/pipelines/rfm_tues.py
+++ dev/pipelines/rfm_tues.py
@@ -1,6 +1,22 @@
-with DAG():
-    rfm_tues__customer_percentage_by_flag_segment_region = Task(
-        task_id = "rfm_tues__customer_percentage_by_flag_segment_region", 
-        component = "Model", 
-        modelName = "rfm_tues__customer_percentage_by_flag_segment_region"
+Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
+SensorSchedule = SensorSchedule(enabled = False)
+
+with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    ecom_orders = Task(
+        task_id = "ecom_orders", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "ecom_orders", "sourceType" : "Table", "sourceName" : "itai.retail_analyst", "alias" : ""}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "instore_sales", "sourceType" : "Table", "sourceName" : "itai.retail_analyst", "alias" : ""}
+    )
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "itai.retail_analyst", "alias" : ""}
     )
