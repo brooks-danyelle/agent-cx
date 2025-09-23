5da6ba502a1ed9cf5fb1104c0c5de1cbbaaa3f25
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-23 23:10:36 +0000

    AI: Added Recency, Frequency, Monetary scores, and RFM segment to pipeline rfm1

--- dev/pipelines/rfm1.py
+++ dev/pipelines/rfm1.py
@@ -1,6 +1,6 @@
 with DAG():
-    rfm1__customer_rfm_analysis = Task(
-        task_id = "rfm1__customer_rfm_analysis", 
+    rfm1__rfm_segmentation_analysis = Task(
+        task_id = "rfm1__rfm_segmentation_analysis", 
         component = "Model", 
-        modelName = "rfm1__customer_rfm_analysis"
+        modelName = "rfm1__rfm_segmentation_analysis"
     )
