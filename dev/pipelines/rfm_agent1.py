d596d3d9fa0903c82ef63ea91f2ab2be8ad0c2d7
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-23 23:04:59 +0000

    User-performed data transformations and changes on pipeline rfm1

--- dev/pipelines/rfm_agent1.py
+++
@@ -1,6 +0,0 @@
-with DAG():
-    rfm_agent1__segment_customer_percentage = Task(
-        task_id = "rfm_agent1__segment_customer_percentage", 
-        component = "Model", 
-        modelName = "rfm_agent1__segment_customer_percentage"
-    )
