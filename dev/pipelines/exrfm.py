9076cd1a34f19e9dd73e9b1c5e0800385279d6bc
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-24 18:37:15 +0000

    AI: join ecom_orders and crm_customers on customer_id for integrated customer order analysis in pipeline exrfm

--- dev/pipelines/exrfm.py
+++ dev/pipelines/exrfm.py
@@ -1,22 +1,6 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
-
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    ecom_orders = Task(
-        task_id = "ecom_orders", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "ecom_orders", "sourceName" : "itai.retail_analyst", "sourceType" : "Table"}
-    )
-    instore_sales = Task(
-        task_id = "instore_sales", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "instore_sales", "sourceName" : "itai.retail_analyst", "sourceType" : "Table"}
-    )
-    crm_customers = Task(
-        task_id = "crm_customers", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "itai.retail_analyst", "alias" : ""}
+with DAG():
+    exrfm__customer_order_join = Task(
+        task_id = "exrfm__customer_order_join", 
+        component = "Model", 
+        modelName = "exrfm__customer_order_join"
     )
