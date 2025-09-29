9e6b8feeb0251fd1f8c064cfbeb16c1256b15f3d
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-09-29 20:16:49 +0000

    User-performed data transformations and changes on pipeline dbx_demo

--- dev/pipelines/test.py
+++
@@ -1,6 +0,0 @@
-with DAG():
-    test__percentage_of_customers_per_segment = Task(
-        task_id = "test__percentage_of_customers_per_segment", 
-        component = "Model", 
-        modelName = "test__percentage_of_customers_per_segment"
-    )
