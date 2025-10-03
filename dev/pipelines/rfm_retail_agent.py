68d99104de2a2c45ca423d45f582ca44dacbe502
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-10-03 15:35:34 +0000

    User-performed data transformations and changes on pipeline rfm_analysis

--- dev/pipelines/rfm_retail_agent.py
+++
@@ -1,6 +0,0 @@
-with DAG():
-    rfm_retail_agent__customer_flag_distribution = Task(
-        task_id = "rfm_retail_agent__customer_flag_distribution", 
-        component = "Model", 
-        modelName = "rfm_retail_agent__customer_flag_distribution"
-    )
