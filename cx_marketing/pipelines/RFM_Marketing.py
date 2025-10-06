103ff3a6bd531f38029a03f7bd7d7c1078c26c96
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-10-06 20:15:45 +0000

    User-performed data transformations and changes on pipeline RFM_Marketing

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -2,4 +2,21 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    pass
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
+    )
+    ecomm_orders = Task(
+        task_id = "ecomm_orders", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "ecomm_orders", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "instore_sales", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
+    )
