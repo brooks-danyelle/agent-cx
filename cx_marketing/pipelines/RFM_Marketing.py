bad63c10d6ad023c0bdf9bc2f2646a2593066998
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-10-06 21:29:23 +0000

    Remove RFM_Marketing__customer_rfm_scores model and its schema from the RFM_Marketing pipeline.

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,6 +1,2 @@
 with DAG():
-    RFM_Marketing__customer_rfm_scores = Task(
-        task_id = "RFM_Marketing__customer_rfm_scores", 
-        component = "Model", 
-        modelName = "RFM_Marketing__customer_rfm_scores"
-    )
+    pass
