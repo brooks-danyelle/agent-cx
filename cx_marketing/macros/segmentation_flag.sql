f5f7bb2b97b4f24d2826a24e4f42fbe53b9dc2f2
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-10-06 20:09:37 +0000

    Initialize cx_marketing project with RFM_Marketing pipeline and supporting macros and sources.

---
+++ cx_marketing/macros/segmentation_flag.sql
@@ -0,0 +1,38 @@
+{% macro segmentation_flag() %}
+CASE
+   -- Best customers: Recent, frequent, and high spenders
+   WHEN recency_score = 5 AND frequency_score = 5 AND monetary_score = 5
+        THEN 'Champion'
+
+
+   -- Loyal customers: buy often, spend decently
+   WHEN frequency_score >= 4 AND monetary_score >= 3
+        THEN 'Loyal'
+
+
+   -- Big spenders: high spenders but not frequent
+   WHEN monetary_score = 5 AND frequency_score <= 3
+        THEN 'High spend but not frequent'
+
+
+   -- New customers: very recent, low frequency and spend
+   WHEN recency_score = 5 AND frequency_score <= 2
+        THEN 'New Customer'
+
+
+   -- At risk: used to spend/buy often but haven’t bought recently
+   WHEN recency_score <= 2 AND (frequency_score >= 4 OR monetary_score >= 4)
+        THEN 'At Risk'
+
+
+   -- Lost customers: inactive, low frequency, low spend
+   WHEN recency_score <= 2 AND frequency_score <= 2 AND monetary_score <= 2
+        THEN 'Lost'
+
+
+   -- Catch-all bucket
+   ELSE 'Needs Attention'
+   END
+{% endmacro %}
+
+ 
