4a6c15bbefa0571cac57c33c8302d41724a8fbdb
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-10-21 14:22:51 +0000

    User-performed data transformations and changes on pipeline RFM_Marketing

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,6 +1,19 @@
 with DAG():
-    RFM_Marketing__rfm_analysis_with_scores = Task(
-        task_id = "RFM_Marketing__rfm_analysis_with_scores", 
-        component = "Model", 
-        modelName = "RFM_Marketing__rfm_analysis_with_scores"
+    ecomm_orders = Task(
+        task_id = "ecomm_orders", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "ecomm_orders", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "instore_sales", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
+    )
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
     )
