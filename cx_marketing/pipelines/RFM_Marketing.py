9b329734e0257adba2d9121ed2c22bb17b4b5504
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-11-04 17:33:24 +0000

    AI: Join crm_customers, ecomm_orders, and instore_sales on customer_id in RFM_Marketing pipeline

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,17 +1,6 @@
 with DAG():
-    instore_sales = Task(
-        task_id = "instore_sales", 
-        component = "Dataset", 
-        table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
-    )
-    crm_customers = Task(
-        task_id = "crm_customers", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "crm_customers", "sourceType" : "Table", "sourceName" : "danyelle.retail", "alias" : ""}
-    )
-    ecomm_orders = Task(
-        task_id = "ecomm_orders", 
-        component = "Dataset", 
-        table = {"name" : "ecomm_orders", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
+    RFM_Marketing__customer_order_sales_data = Task(
+        task_id = "RFM_Marketing__customer_order_sales_data", 
+        component = "Model", 
+        modelName = "RFM_Marketing__customer_order_sales_data"
     )
