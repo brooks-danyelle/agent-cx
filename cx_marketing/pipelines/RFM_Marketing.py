d33d50555889974164f28ec9c7f7a6d36cd7f176
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-11-04 15:44:36 +0000

    Remove `RFM_Marketing__Reformat_1` transformation from `RFM_Marketing` pipeline and update dataset tasks

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,6 +1,16 @@
 with DAG():
-    RFM_Marketing__Reformat_1 = Task(
-        task_id = "RFM_Marketing__Reformat_1", 
-        component = "Model", 
-        modelName = "RFM_Marketing__Reformat_1"
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
+    )
+    ecomm_orders = Task(
+        task_id = "ecomm_orders", 
+        component = "Dataset", 
+        table = {"name" : "ecomm_orders", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
+    )
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        table = {"name" : "crm_customers", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
     )
