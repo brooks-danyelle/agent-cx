2c2634c65b9516ab760a9705fde87aa948cf492f
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-11-04 16:17:29 +0000

    Remove `RFM_Marketing__customer_rfm_scores` transformation from `RFM_Marketing` pipeline

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,6 +1,16 @@
 with DAG():
-    RFM_Marketing__customer_rfm_scores = Task(
-        task_id = "RFM_Marketing__customer_rfm_scores", 
-        component = "Model", 
-        modelName = "RFM_Marketing__customer_rfm_scores"
+    crm_customers = Task(
+        task_id = "crm_customers", 
+        component = "Dataset", 
+        table = {"name" : "crm_customers", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
+    )
+    instore_sales = Task(
+        task_id = "instore_sales", 
+        component = "Dataset", 
+        table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
+    )
+    ecomm_orders = Task(
+        task_id = "ecomm_orders", 
+        component = "Dataset", 
+        table = {"name" : "ecomm_orders", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
     )
