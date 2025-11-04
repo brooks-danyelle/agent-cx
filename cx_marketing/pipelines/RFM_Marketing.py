5d0b946808e0267a48111c2bd244ef1b39e87dee
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2025-11-04 17:33:10 +0000

    Remove redundant dataset tasks from `RFM_Marketing` pipeline

--- cx_marketing/pipelines/RFM_Marketing.py
+++ cx_marketing/pipelines/RFM_Marketing.py
@@ -1,26 +1,8 @@
 with DAG():
-    crm_customers_1 = Task(
-        task_id = "crm_customers_1", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "crm_customers", "sourceName" : "danyelle.retail", "sourceType" : "Table"}
-    )
-    instore_sales_1 = Task(
-        task_id = "instore_sales_1", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "instore_sales", "sourceName" : "danyelle.retail", "sourceType" : "Table"}
-    )
     instore_sales = Task(
         task_id = "instore_sales", 
         component = "Dataset", 
         table = {"name" : "instore_sales", "sourceType" : "Source", "sourceName" : "danyelle.retail"}
-    )
-    ecomm_orders_1 = Task(
-        task_id = "ecomm_orders_1", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "ecomm_orders", "sourceName" : "danyelle.retail", "sourceType" : "Table"}
     )
     crm_customers = Task(
         task_id = "crm_customers", 
